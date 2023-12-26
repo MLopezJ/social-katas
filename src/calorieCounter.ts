7401543a75a206e25a0592ac2e2daaa720422368
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-12-26 02:39:33 +0100

    fix: prettier

--- src/calorieCounter.ts
+++ src/calorieCounter.ts
@@ -2,15 +2,16 @@
  * Select elve whith more calories
  */
 export const calorieCounter = (txt: string) => {
-    const readFromPath = '' // get txt
-    const calories = [11000, 6000, 4000,  10000, 24000] // read form text and transform to number
-    const max = getMax(calories)
+	const readFromPath = '' // get txt
+	const calories = [11000, 6000, 4000, 10000, 24000] // read form text and transform to number
+	const max = getMax(calories)
 }
 
 /**
  * Get bigger number from list
  */
-const getMax = (input: number[]): number => input.reduce((previus, current) => {
-    if (previus > current) return previus
-    return current
-})
+const getMax = (input: number[]): number =>
+	input.reduce((previus, current) => {
+		if (previus > current) return previus
+		return current
+	})
