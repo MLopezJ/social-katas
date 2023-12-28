caeb4a41b7d061359e81a7201c3b6d466b9c5c7c
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-12-28 20:09:58 +0100

    WIP: calorie counter

--- src/oscarLopez/calorieCounter.ts
+++ src/oscarLopez/calorieCounter.ts
@@ -1,14 +1,45 @@
-import { read } from "./read"
+import { read } from './read'
 
+const sum = (input: number[]) => {
+	const x = input.reduce((previous, current) => {
+		return previous + current
+	})
+	
+	return x
+}
 /**
  * Select elve whith more calories
  */
 export const calorieCounter = async (txt: string) => {
 	const readFromPath = await read(txt)
-	const calories = [11000, 6000, 4000, 10000, 24000] // read form text and transform to number
+	// add undefined at the end of the array
+	readFromPath.push(undefined)
+	
+	const x = readFromPath.reduce(
+		(previous, current) => {
+			
+			if (current === undefined) {
+				const result = sum(previous.accumulator)
+				//console.log({result})
+				return {
+					numbers: [...previous.numbers, result],
+					accumulator: [],
+				}
+			}
+			
+			return {
+				numbers: previous.numbers,
+				accumulator: [current, ...previous.accumulator],
+			}
+		},
+		{ numbers: [], accumulator: [] },
+	)
+	console.log(x)
+	const calories = x.numbers //[11000, 6000, 4000, 10000, 24000] // read form text and transform to number
 	const max = getMax(calories)
-    return max
+	return max
 }
+
 
 /**
  * Get bigger number from list
