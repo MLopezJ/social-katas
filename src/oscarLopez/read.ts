213285166f627657b9fa1e131cabcf49b8d77a5f
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-12-28 19:40:47 +0100

    fix: read file
    read file and transform string to number or undefined depending of the case

--- src/oscarLopez/read.ts
+++ src/oscarLopez/read.ts
@@ -2,13 +2,16 @@ import { readFile } from 'node:fs/promises'
 import path from 'node:path'
 
 /**
- * Get tokens from calibrationDocument.txt
+ * read file and transform string to number or undefined depending of the case
  */
-export const read = async (file: string) => {
+export const read = async (file: string): Promise<(number | undefined)[]> => {
 	const baseDir = process.cwd()
 	const subDir = (...tree: string[]): string => path.join(baseDir, ...tree)
 	return readFile(subDir('src/oscarLopez', file), 'utf-8').then((result) => {
-		result.toString().split('\n').map(x => console.log(x.length))
-		return result.toString().split('\n')
+		const data = result.toString().split('\n').map(x => {
+			if (x.length > 0) return Number(x)
+			return undefined
+		})
+		return data
 	})
 }
