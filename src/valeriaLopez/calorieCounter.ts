f16bca3f91770c5e7c82b1cf7c57b808cafcc765
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-12-29 00:58:15 +0100

    WIP: sum top 3

--- src/valeriaLopez/calorieCounter.ts
+++ src/valeriaLopez/calorieCounter.ts
@@ -2,15 +2,18 @@
  * Select the top 3 elves whith more calories and sum the total of it
  */
 export const calorieCounter = async (txt: string) => {
-    // read logs 
+	// read logs
 
-    // sum total amount of calories per elve
-    const calories = [6000, 4000, 11000, 24000, 10000]
+	// sum total amount of calories per elve
+	const calories = [6000, 4000, 11000, 24000, 10000]
 
-    // select top 3 (higher number) from calories
-    const top3 = [11000, 24000, 10000]
+	// select top 3 (higher number) from calories
+	const top3 = [11000, 24000, 10000]
 
-    // sum of top 3
-    const result = 45000
+	// sum of top 3
+	const result = top3.reduce((previous, current) => {
+		return previous + current
+	})
 
+	return result
 }
