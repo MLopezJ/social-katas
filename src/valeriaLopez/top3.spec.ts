42605d6fc8c28b9e1db8646eb00a87e1c85e613f
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-12-29 01:32:44 +0100

    test: failing test
    rerturn a list with the 3 higher numbers in total

--- src/valeriaLopez/top3.spec.ts
+++ src/valeriaLopez/top3.spec.ts
@@ -10,6 +10,9 @@ void describe('top3', () => {
 
         // no comparation is needed
 		[[1, 2, 3], 0, [1, 2, 3]],
+
+        // 
+        [[4000, 11000, 24000], 10000, [[11000, 24000, 10000]]]
 	] as [number[], number, number[]][]) {
 		void it(`should return a list with the 3 higher elements`, () => {
 			assert.deepEqual(top3(list, number), expected)
