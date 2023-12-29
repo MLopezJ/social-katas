826e4163f24db474541a7624a1a5d8a6582554da
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-12-29 22:57:55 +0100

    feat: read logs

--- src/valeriaLopez/calorieCounter.ts
+++ src/valeriaLopez/calorieCounter.ts
@@ -1,8 +1,12 @@
+import { transformLogs } from "src/oscarLopez/calorieCounter.js"
+import { read } from "src/oscarLopez/read.js"
+
 /**
  * Select the top 3 elves whith more calories and sum the total of it
  */
 export const calorieCounter = async (txt: string) => {
 	// read logs
+    const logs = await read(txt)
 
 	// sum total amount of calories per elve
 	const calories = [6000, 4000, 11000, 24000, 10000]
