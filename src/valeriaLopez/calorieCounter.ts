2bb4c9923ff7e64a22e3235fe2229de57974ecf0
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-12-29 23:22:59 +0100

    feat: calorie counter
    Select the top 3 elves whith more calories and sum the total of it

--- src/valeriaLopez/calorieCounter.ts
+++ src/valeriaLopez/calorieCounter.ts
@@ -1,21 +1,24 @@
-import { transformLogs } from "src/oscarLopez/calorieCounter.js"
-import { read } from "src/oscarLopez/read.js"
+import { transformLogs } from 'src/oscarLopez/calorieCounter.js'
+import { read } from 'src/oscarLopez/read.js'
+import { top3 } from './top3.js'
 
 /**
  * Select the top 3 elves whith more calories and sum the total of it
  */
 export const calorieCounter = async (txt: string) => {
 	// read logs
-    const logs = await read(txt)
+	const logs = await read(txt)
 
 	// sum total amount of calories per elve
 	const calories = transformLogs(logs)
 
 	// select top 3 (higher number) from calories
-	const top3 = [11000, 24000, 10000]
+	const top = calories.reduce((previous: number[], current) => {
+		return top3(previous, current)
+	}, [])
 
 	// sum of top 3
-	const result = top3.reduce((previous, current) => {
+	const result = top.reduce((previous, current) => {
 		return previous + current
 	})
 
