a09d4a0182379f7e522aa2748c1e86fc6cb4204f
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2024-02-07 23:27:06 +0100

    WIP: calculate step

--- src/felipeMartinez/index.ts
+++ src/felipeMartinez/index.ts
@@ -11,9 +11,26 @@ export const parser = (input: string): matrix => input.split('\n').map(element =
  * and then count the amount of dots in the matrix
  */
 export const main = (input: string): number => {
+    // transform data
 	const matrix = parser(input)
+
 	// order hash tags
+    orderHashtags(matrix)
 
 	// count dots
 	return 0
+}
+
+/**
+ * 
+ */
+const orderHashtags = (input: matrix) => {
+    calculateStep(input)
+}
+
+/**
+ * 
+ */
+const calculateStep = (input: matrix, ) => {
+ // TODO
 }
